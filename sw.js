538849e42bf4519dac8f77aa6e834534dcaae0e5
lihan372923-collab <lihan372923-collab@users.noreply.github.com>
2026-06-03 23:33:16 +0800

    Add icons, fix manifest, update SW

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE="it-quest-final-v1";const ASSETS=["./","./index.html","./style.css","./data.js","./app.js","./manifest.json"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="it-quest-final-v2";
+const ASSETS=["./","./index.html","./style.css","./data.js","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
